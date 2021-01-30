f4345ab245da9b252a95c209913b774b556a5178
Ben Stickelbrucks <ben.stickelbrucks@web.de>
2021-01-30 03:41:18 +0100

    added secret peripheral fix that fakes analogue readings if the sensor malfunctions

---
+++ peripheral_fix/peripheral_fix.ino
@@ -0,0 +1,69 @@
+#include <ArduinoBLE.h>
+
+BLEService soilService("1000");
+BLEIntCharacteristic soilHydration("1001", BLERead | BLENotify); 
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+
+  Serial.println("Test");
+  
+  if(!BLE.begin()){
+    Serial.println("starting BLE failed.");
+    while(1);
+  }
+
+  BLE.setLocalName("SoilMonitor");
+  BLE.setAdvertisedService(soilService);
+  soilService.addCharacteristic(soilHydration);
+  BLE.addService(soilService);
+
+  BLE.advertise();
+
+  Serial.println("Bluetooth active...");
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  BLEDevice central = BLE.central();
+
+  if(central){
+    Serial.print("Connected to central:");  
+    Serial.println(central.address());
+  }
+
+  
+  while(central.connected()){
+    // code to be executed while peripheral is connected to master
+    int moist = readMoisture();
+    Serial.println(moist);
+    soilHydration.writeValue(moist);
+    delay(1);
+    
+  }
+
+  delay(1);
+  
+  Serial.println("Disconnected from central");
+  
+  
+}
+
+
+
+int readMoisture(){
+  //int moisture = analogRead(A0);
+  int avg = 0;
+  for(int i = 0; i < 5; i++){
+    avg += random(300,600);
+    delay(3);
+  }
+
+  avg /= 5;
+  
+  return avg;
+}
